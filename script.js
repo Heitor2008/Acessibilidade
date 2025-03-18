3ea3f3ddbc609e0b0756587b97bbeea2e05705a4
Heitor2008 <125297810+Heitor2008@users.noreply.github.com>
2025-03-18 13:36:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   const aumentaFonteBotao document.getElementByID('aumentar-fonte');
   const diminuiFonteBotao document.getElementByID('diminuir-fonte');
+  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
   let tamanhoAtualFonte = 1;
   aumentaFonteBotao.addEventListener('click', function(){
@@ -14,4 +16,10 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+  botaoDeAcessibilidade.addEventListener('click', function (){
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+})
+  
 })
